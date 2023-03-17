dbdf1feae11d3805d0161170eec8f31b95fff0c5
Diego Cambiaso <diegocambiaso@gmail.com>
2023-03-17 12:42:34 -0300

    Update donwload-video.py

--- donwload-video.py
+++ donwload-video.py
@@ -1,3 +1,13 @@
+#!/usr/bin/env python
+# coding=utf8
+#
+# Copyright (C) 2023, Diego Cambiaso
+# GNU General Public License v3.0
+
+'''
+This script is for educational purposes and will let you danload a video.
+'''
+
 from pytube import YouTube
 
 def DownloadVideo(url):
