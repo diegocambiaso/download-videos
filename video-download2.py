37676b9dd16cf27da4d33645e6f3765078899db8
Diego Cambiaso <diegocambiaso@gmail.com>
2023-04-18 21:41:50 -0500

    Create video-download2.py

---
+++ video-download2.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding = utf8
+#
+# Copyright (C) 2023, Diego Cambiaso
+# GNU General Public License v3.0
+
+# Version: 2
+
+'''
+This script is for educational purposes and will let you download a video.
+'''
+
+from pytube import YouTube
+import pyperclip as pc
+
+def DownloadVideo(url):
+    try:
+        youtubeObject = YouTube(url)
+        youtubeObject = youtubeObject.streams.get_highest_resolution()
+        print("Downloading...")
+        youtubeObject.download()
+    except(OSError, RuntimeError, ValueError):
+        print("An error has occurred")
+    except Exception as e:
+        print("An error has ocurred.")
+        e.add_note("Unknowed error trying donwloading the video")
+        print("Error: ", e)
+    except KeyboardInterrupt:
+        print("Stoped by the user")
+    else:
+        print("Download is completed successfully")
+
+if __name__ == "__main__":
+    print("Download Video")
+    print("-"*20)
+    
+    resp = "youtube.com" in pc.paste()
+    
+    if resp:
+        url = pc.paste()
+        print(f"Do you want to download: {url}")
+        a = input("Y/n: ")
+        if a.upper() == "Y":
+            DownloadVideo(url)
+    else:
+        url = input("Write (or ctrl+v) the video URL: ")
+        DownloadVideo(url)
+    
+    print("Finished")
+    
